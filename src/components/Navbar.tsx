a2888dd7247d282acc0ada4790d9b8f0647a6f41
radebeneo <radebeneo17@gmail.com>
2025-11-22 23:06:11 +0200

    added porfolio, contact and projects navLinks to navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,17 @@ const Navbar = () => {
             <div>
                 <img src="/images/logo.svg" alt="logo" />
                 <p className="font-bold">umkhulubhungane</p>
-
+                <ul>
+                    {[
+                        {id: 1, name: "Portfolio"},
+                        {id: 2, name: "Contact"},
+                        {id: 3, name: "Projects"},
+                    ].map(({id, name}) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </nav>
     )
