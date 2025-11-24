228ee3bd77207b555cc47fa98123fd9f23a79558
radebeneo <radebeneo17@gmail.com>
2025-11-24 13:19:17 +0200

    added macOS icons to navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import {navLinks} from "#constants";
+import {navIcons, navLinks} from "#constants";
 
 const Navbar = () => {
     return (
@@ -10,6 +10,15 @@ const Navbar = () => {
                     {navLinks.map(({id, name}) => (
                         <li key={id}>
                             <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {navIcons.map(({id, img}) => (
+                        <li key={id}>
+                            <img src={img} className="icon-hover" alt={`icon-${id}`}/>
                         </li>
                     ))}
                 </ul>
