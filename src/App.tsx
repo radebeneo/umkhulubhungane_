37bbc9f1c545008f2754c7ed19c80fd03304fda7
radebeneo <radebeneo17@gmail.com>
2025-11-25 19:47:32 +0200

    wrapped terminal component with WindowWrapper higher order component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import {Navbar, Welcome, Dock} from "#components";
-import gsap from "gsap";
 
+import gsap from "gsap";
 import { Draggable} from "gsap/Draggable";
+
+import {Navbar, Welcome, Dock} from "#components";
+import {Terminal} from "#windows";
+
 gsap.registerPlugin(Draggable);
 
 const App = () => {
@@ -10,6 +13,8 @@ const App = () => {
            <Navbar/>
            <Welcome/>
            <Dock/>
+
+           <Terminal />
        </main>
     )
 }
---
+++ src/windows/Terminal.tsx
@@ -0,0 +1,21 @@
+import WindowWrapper from "#hoc/WindowWrapper";
+
+const Terminal = () => {
+    return (
+        <>
+            <div id="window-header">
+                <p>Window Controls</p>
+                <h2>Tech Stack</h2>
+            </div>
+            <div className="techstack">
+                <p>
+                    <span className="font-bold">@umkhulubhungane ~ % </span>
+                    show tech stack
+                </p>
+            </div>
+        </>
+    )
+}
+
+const TerminalWindow = WindowWrapper(Terminal, "terminal")
+export default TerminalWindow
