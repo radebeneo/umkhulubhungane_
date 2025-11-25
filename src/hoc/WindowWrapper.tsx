12a1bd67fbf5fe68967c432e6ad03fdf35562dd4
radebeneo <radebeneo17@gmail.com>
2025-11-25 20:47:42 +0200

    useLayout effect to open and close terminal window

--- src/hoc/WindowWrapper.tsx
+++ src/hoc/WindowWrapper.tsx
@@ -1,11 +1,25 @@
 import useWindowStore from "#store/window";
-import {useRef} from "react";
+import {useLayoutEffect, useRef} from "react";
+import {useGSAP} from "@gsap/react";
 
 const WindowWrapper = (Component, windowKey) => {
     const Wrapped = (props) => {
         const { focusWindow, windows } = useWindowStore()
         const { isOpen, zIndex} = windows[windowKey]
         const ref = useRef(null);
+
+        useGSAP(() => {
+            const el = ref.current
+            if(!el || !isOpen) return;
+            el.style.display = "block"
+        }, [isOpen])
+
+        useLayoutEffect(() => {
+            const el = ref.current
+            if(!el) return
+            el.style.display = isOpen ? "block" : "none"
+        }, [isOpen]);
+
 
         return (
             <section  id={windowKey} ref={ref} className="absolute" style={{zIndex}}>
