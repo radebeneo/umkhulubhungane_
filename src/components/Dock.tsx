9d74a0965b64ddf47d7a3d2d4a61b3a6754fc72c
radebeneo <radebeneo17@gmail.com>
2025-11-25 12:40:55 +0200

    installed react tooltip

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,4 +1,5 @@
 import {useRef} from "react";
+import { Tooltip} from "react-tooltip";
 import {dockApps} from "#constants";
 
 const Dock = () => {
@@ -13,8 +14,9 @@ const Dock = () => {
                    <button type="button" className="dock-icon" aria-label={name} data-tooltip-id="dock-tooltip" data-tooltip-content={name} data-tooltip-delay-show={150} disabled={!canOpen} onClick={() => toggleApp({id, canOpen})}>
                         <img src={`/images/${icon}`} alt={name} loading="lazy" className={canOpen ? "" : "opacity-60"}/>
                    </button>
-               </div>)}
-
+               </div>
+               )}
+               <Tooltip id="dock-tooltip" place="top" className="tooltip" />
            </div>
         </section>
     )
