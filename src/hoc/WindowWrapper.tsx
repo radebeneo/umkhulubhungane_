432c50c96f7ba2b1978615421d220caea72e0ba4
radebeneo <radebeneo17@gmail.com>
2025-11-25 20:53:39 +0200

    Open and close window animation using gsap.

--- src/hoc/WindowWrapper.tsx
+++ src/hoc/WindowWrapper.tsx
@@ -1,6 +1,7 @@
 import useWindowStore from "#store/window";
 import {useLayoutEffect, useRef} from "react";
 import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const WindowWrapper = (Component, windowKey) => {
     const Wrapped = (props) => {
@@ -12,6 +13,8 @@ const WindowWrapper = (Component, windowKey) => {
             const el = ref.current
             if(!el || !isOpen) return;
             el.style.display = "block"
+
+            gsap.fromTo(el, {scale: 0.8, opacity: 0, y: 40}, {scale: 1, opacity: 1, y: 0, duration: 0.5, ease: "power3.out"})
         }, [isOpen])
 
         useLayoutEffect(() => {
