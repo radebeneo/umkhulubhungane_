fd2f682d5d5037c868c7d7b2a11d9722c9947ad7
radebeneo <radebeneo17@gmail.com>
2025-11-25 18:02:36 +0200

    added safe guards for non-existence windows inside toggleApp method.

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -53,6 +53,11 @@ const Dock = () => {
 
         const window = windows[app.id];
 
+        if(!window) {
+            console.error(`Window not found for app: ${app.id} `);
+            return;
+        }
+
         if (window.isOpen) {
             closeWindow(app.id);
         } else {
